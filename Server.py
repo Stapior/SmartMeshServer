c4f1818b1952b078656f25ec96006441489246c6
Piotr <sensaj10@gmail.com>
2020-04-18 13:28:07 +0200

    Rozszerzenie serwera

--- Server.py
+++ Server.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-from flask import Flask, current_app, g, jsonify, make_response, request
+from flask import Flask, current_app, g, jsonify, request
 from flask_mqtt import Mqtt
 import json
 import logging
 import sqlite3
-import sys
 
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = '127.0.0.1'
@@ -16,6 +15,10 @@ mqtt = Mqtt(app)
 logging.basicConfig(level=logging.DEBUG)
 
 
+def log(msg):
+    app.logger.info(msg)
+
+
 def make_dicts(cursor, row):
     return dict((cursor.description[idx][0], value)
                 for idx, value in enumerate(row))
@@ -23,7 +26,7 @@ def make_dicts(cursor, row):
 
 def get_db():
     if 'db' not in g:
-        app.logger.info(current_app.config['DATABASE'])
+        log(current_app.config['DATABASE'])
         g.db = sqlite3.connect("./mesh-home.sqlite",
                                detect_types=sqlite3.PARSE_DECLTYPES)
         g.db.row_factory = make_dicts
@@ -66,8 +69,45 @@ def getObject(objectId):
     return json.dumps(cur.fetchone())
 
 
+@app.route('/addOwnObject')
+def addOwnObject():
+    cur = get_db().cursor()
+    objectType = request.args.get('type', default='switch')
+    name = request.args.get('name', default='Sztuczny obiekt')
+    description = request.args.get('description', default='Sztuczny obiekt')
+    cur.execute("SELECT min(node_id) as nodeId from objects where node_id < 0 ")
+    res = cur.fetchone()
+    lastNodeId = res.get("nodeId")
+    if lastNodeId is None:
+        lastNodeId = 0
+    newNodeId = lastNodeId - 1
+    cur.execute("Insert into objects (node_id, internal_id, type, name, description) values (?, 1, ?, ?, ?)",
+                (newNodeId, objectType, name, description))
+    get_db().commit()
+
+    cur.execute("SELECT * FROM objects where node_id  = ?", (newNodeId, ))
+    obj = cur.fetchone()
+
+    return jsonify(objectId=obj["object_id"])
+
+
 @app.route('/change/<objectId>/<value>')
-def changeState(objectId, value):
+def changeState(objectId, value, db=None):
+    if db is None:
+        db = get_db()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM objects where object_id = ?", objectId)
+    obj = cur.fetchone()
+    msg = {"nodeId": int(obj["node_id"]),
+           "objectId": int(obj["internal_id"]),
+           "type": "change",
+           "value": int(value)}
+    mqtt.publish("painlessMesh/to/" + str(obj["node_id"]), json.dumps(msg))
+    return jsonify(success=True)
+
+
+@app.route('/read/<objectId>')
+def readState(objectId, value):
     cur = get_db().cursor()
     cur.execute("SELECT * FROM objects where object_id = ?", objectId)
     obj = cur.fetchone()
@@ -102,29 +142,31 @@ def updateObject(objectId):
     obj = cur.fetchone()
     name = request.args.get('name', default=obj["name"])
     description = request.args.get('description', default=obj["description"])
-    cur.execute("Update objects set name = ?, description = ? where object_id = ? ", (name, description, objectId))
+    objType = request.args.get('type', default=obj["type"])
+    cur.execute("Update objects set name = ?, description = ?, type = ? where object_id = ? ",
+                (name, description, objType, objectId))
     get_db().commit()
     return jsonify(success=True)
 
 
 @mqtt.on_connect()
 def on_connect(client, userdata, flags, rc):
-    app.logger.info("Connected with result code " + str(rc))
+    log("Connected with result code " + str(rc))
     mqtt.subscribe("painlessMesh/from/#")
 
 
-def saveValue(data):
+def saveValue(data, db):
+    log(">>saveValue")
     db = get_mqtt_db()
     cur = db.cursor()
     cur.execute(
-        'Update reads set value = ? where object_id = (SELECT object_id from objects where node_id =? and internal_id = ? )',
+        'Update objects set value = ? where node_id =? and internal_id = ? ',
         (data["value"], data["nodeId"], data["objectId"]))
     db.commit()
-    db.close()
+    log("saveValue<<")
 
 
-def checkAndAddToDatabase(data):
-    db = get_mqtt_db()
+def checkAndAddToDatabase(data, db):
     cur = db.cursor()
     cur.execute("SELECT count(*) as cnt from objects where node_id = ? and internal_id = ?",
                 (data["nodeId"], data["objectId"]))
@@ -132,11 +174,10 @@ def checkAndAddToDatabase(data):
         cur.execute("Insert into objects (node_id, internal_id, type) values (?, ?, ?)",
                     (data["nodeId"], data["objectId"], data.get("objectType", "switch")))
         db.commit()
-    db.close()
+        log(">>new Object add<<")
 
 
-def saveRead(data):
-    db = get_mqtt_db()
+def saveRead(data, db):
     cur = db.cursor()
     cur.execute("SELECT object_id from objects where node_id =? and internal_id = ?",
                 (data["nodeId"], data["objectId"]))
@@ -145,47 +186,53 @@ def saveRead(data):
         'Insert into reads (object_id, value) values (?,?)',
         (obj["object_id"], data["value"]))
     db.commit()
-    db.close()
 
-def saveEvent(data):
-    db = get_mqtt_db()
+
+def eventHandler(data, db):
     cur = db.cursor()
     cur.execute("SELECT object_id from objects where node_id =? and internal_id = ?",
                 (data["nodeId"], data["objectId"]))
     obj = cur.fetchone()
+    objId = obj["object_id"]
     cur.execute(
         'Insert into events (object_id, value) values (?,?)',
-        (obj["object_id"], data["value"]))
+        (objId, data["value"]))
+
+    processEvent(objId, db)
     db.commit()
-    db.close()
+
+
+def processEvent(objectId, db):
+    cur = db.cursor()
+    cur.execute("Select * from scenarios where from_object_id = ?", objectId)
+    steps = cur.fetchall()
+    for step in steps:
+        targetObjectId = step["to_object"]
+        value = step["new_value"]
+        changeState(targetObjectId, value, db)
 
 
 @mqtt.on_message()
 def on_message(client, userdata, msg):
+    db = get_mqtt_db()
     try:
-        app.logger.info("Message")
+        log(">>New mqtt message")
         jsonString = msg.payload.decode("utf-8")
         data = json.loads(jsonString)
-        app.logger.info(data)
-        checkAndAddToDatabase(data)
-        nodeId = data["nodeId"]
-        internalId = data["objectId"]
+        log(data)
+        checkAndAddToDatabase(data, db)
 
         type = data["type"]
         if type == "read-value":
-            app.logger.info(f'readed value {data["value"]}')
-            saveValue(data)
-        if type == "new-value":
-            app.logger.info(f'readed value {data["value"]}')
-            saveRead(data)
-        if type == "event":
-            app.logger.info(f'readed value {data["value"]}')
-            saveEvent(data)
+            saveValue(data, db)
+        elif type == "new-value":
+            saveRead(data, db)
+        elif type == "event":
+            eventHandler(data, db)
         elif type == "hello":
-            app.logger.info(f'hello value {data["value"]}')
-
-    except KeyError as e:
-        app.logger.error(e)
+            checkAndAddToDatabase(data, db)
+    finally:
+        db.close()
 
 
 if __name__ == '__main__':
